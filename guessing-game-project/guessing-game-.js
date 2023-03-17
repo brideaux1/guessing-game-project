a3cb87de976e9d2908d3768881c107078ef4d90b
Britney <britney.rideaux@yahoo.com>
2023-03-16 23:23:11 -0500

    changes to notes

--- guessing-game-project/guessing-game-.js
+++ guessing-game-project/guessing-game-.js
@@ -1,6 +1,22 @@
 // Import the readline module.
-const readline = require("readline");
 // Create the interface for input and output.
+//initialize a variable "secretNumber" to any positive number (change to get a number at random)
+// create a function called checkGuess that accepts a number
+//  when the argument is larger than secretNumber, it should print 'Too high.' and return false
+//  when the argument is smaller than secretNumber, it should print 'Too low.' and return false
+//  when the argument is equal to secretNumber, it should print 'Correct!' and return true
+// console.log(checkGuess(50)); // "Correct!" true
+// console.log(checkGuess(10)); // "Too low." false
+// console.log(checkGuess(75)); // "Too high." false
+
+// Define a function called askGuess
+// Use readline module question to ask the user to "Enter a guess:"
+// once the user enters "num", checkGuess function should be called with their number as an argument and the interface should be closed.
+// If guess is correct, it will print "You win!" if incorrect it will call the askGuess function again
+//
+
+const readline = require("readline");
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -10,12 +26,7 @@ function randomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-//initialize a variable "secretNumber" to any positive number (change to get a number at random)
 let secretNumber = randomInRange(0, 100);
-// create a function called checkGuess that accepts a number
-//  when the argument is larger than secretNumber, it should print 'Too high.' and return false
-//  when the argument is smaller than secretNumber, it should print 'Too low.' and return false
-//  when the argument is equal to secretNumber, it should print 'Correct!' and return true
 
 function checkGuess(num) {
   if (num > secretNumber) {
@@ -31,15 +42,6 @@ function checkGuess(num) {
     return true;
   }
 }
-// console.log(checkGuess(50)); // "Correct!" true
-// console.log(checkGuess(10)); // "Too low." false
-// console.log(checkGuess(75)); // "Too high." false
-
-// Define a function called askGuess
-// Use readline module question to ask the user to "Enter a guess:"
-// once the user enters "num", checkGuess function should be called with their number as an argument and the interface should be closed.
-// If guess is correct, it will print "You win!" if incorrect it will call the askGuess function again
-//
 
 function askGuess() {
   rl.question("Enter a guess: ", (num) => {
