2cf72224e1684f0108176f99d3a4dfaedfd8dff6
Britney <britney.rideaux@yahoo.com>
2023-03-16 23:11:18 -0500

    finished guessing game

--- guessing-game-project/guessing-game-.js
+++ guessing-game-project/guessing-game-.js
@@ -7,11 +7,11 @@ const rl = readline.createInterface({
 });
 
 function randomInRange(min, max) {
-  return Math.floor(Math.random() * max - min + 1) + min;
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 //initialize a variable "secretNumber" to any positive number (change to get a number at random)
-const secretNumber = randomInRange(0, 100);
+let secretNumber = randomInRange(0, 100);
 // create a function called checkGuess that accepts a number
 //  when the argument is larger than secretNumber, it should print 'Too high.' and return false
 //  when the argument is smaller than secretNumber, it should print 'Too low.' and return false
@@ -20,7 +20,6 @@ const secretNumber = randomInRange(0, 100);
 function checkGuess(num) {
   if (num > secretNumber) {
     console.log("Too high.");
-    no;
     return false;
   }
   if (num < secretNumber) {
@@ -52,4 +51,21 @@ function askGuess() {
     }
   });
 }
-askGuess();
+// askGuess();
+
+function askRange() {
+  rl.question("Enter a minimum number: ", (min) => {
+    rl.question("Enter a maximum number: ", (max) => {
+      console.log(
+        "I'm thinking of a number between " +
+          min +
+          " and " +
+          max +
+          ". Time to guess!"
+      );
+      secretNumber = randomInRange(Number(min), Number(max));
+      askGuess();
+    });
+  });
+}
+askRange();
